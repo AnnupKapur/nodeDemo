6a328f33dc59c575946e2d43162feab6237d5af7
AnnupKapur <AnnupKapur@gmail.com>
2022-02-17 00:21:02 +0000

    gitignore file

---
+++ 1-intro.js
@@ -0,0 +1,8 @@
+var intAmount = 9;
+if (intAmount < 10) {
+    console.log('Less than 10');
+}
+else {
+    console.log('10 or more');
+}
+console.log('Hello world!');
--- 1-intro.ts
+++
@@ -1,9 +0,0 @@
-const intAmount = 9;
-
-if(intAmount < 10){
-	console.log('Less than 10')
-} else {
-	console.log('10 or more')
-}
-
-console.log('Hello world!');
